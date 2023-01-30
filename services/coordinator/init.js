b6e1375bd0a8bdf741587cd90d255fdf16ae49bf
Farruco Sanjurjo <madtrick@gmail.com>
2023-01-30 13:56:32 +0100

    Dummy counter can be visualized in Graphite

--- services/coordinator/init.js
+++ services/coordinator/init.js
@@ -1,1 +1,10 @@
+const Statsd = require('statsd-client')
+
 console.log("Starting the coordinator...")
+
+const statsdClient = new Statsd({ host: 'graphite-statsd' })
+
+setInterval(() => {
+  console.log('Sending metric');
+  statsdClient.increment('some.counter')
+}, 1000)
