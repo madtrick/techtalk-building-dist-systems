3bf05879430a52a3f2ef28924558c1ebccdc9620
Farruco Sanjurjo <madtrick@gmail.com>
2023-02-01 17:58:24 +0100

    Small revamp of the docker statsd tool. Include grafana

--- tools/docker-stats-statsd/init.js
+++ tools/docker-stats-statsd/init.js
@@ -4,22 +4,24 @@ const Statsd = require('statsd-client')
 const docker = new Docker({ socketPath: '/var/run/docker.sock' })
 const statsdClient = new Statsd({ host: 'graphite-statsd' })
 
-// List
-docker.container.list()
-   // Inspect
-  .then(containers => containers[0].status())
-  .then(container => container.stats())
-  .then(stats => {
+async function main () {
+  const containerList = await docker.container.list()
+  const monitoredContainers = containerList.filter((c) => {
+    return c.data.Names.includes('/coordinator')
+  })
+
+  for (const container of monitoredContainers) {
+    const status = await container.status()
+    const stats = await status.stats()
+
     stats.on('data', buffer => {
       const stat = JSON.parse(buffer)
-
-      if (stat.name !== '/coordinator') {
-        return
-      }
 
       console.log('Memory usage', stat.memory_stats.usage)
       statsdClient.gauge('memory', stat.memory_stats.usage)
     })
     stats.on('error', err => console.log('Error: ', err))
-  })
-  .catch(error => console.log(error))
+  }
+}
+
+main()
