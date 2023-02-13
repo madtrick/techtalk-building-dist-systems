6e2969c3fbdde2d529f45c840270076f4be5f9dd
Farruco Sanjurjo <madtrick@gmail.com>
2023-02-13 16:50:20 +0100

    Add K6 scenarios

--- k6-scenarios/k6-scenario.js
+++ k6-scenarios/k6-scenario.js
@@ -1,7 +1,25 @@
 import http from 'k6/http'
 import { sleep } from 'k6'
 
+export const options = {
+  scenarios: {
+    first_wave: {
+      executor: 'per-vu-iterations',
+      vus: 100,
+      iterations: 50,
+      maxDuration: '30s'
+    },
+    second_wave: {
+      startTime: '10s',
+      executor: 'constant-arrival-rate',
+      preAllocatedVUs: 200,
+      rate: 25,
+      timeUnit: '1s',
+      duration: '20s'
+    }
+  },
+}
+
 export default function () {
   http.get('http://coordinator:3000')
-  sleep(1)
 }
